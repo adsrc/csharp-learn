d829d2c0c19194ff8029891a16d19f3cb9412ed9
achetan40 <aditya16217@iiitd.ac.in>
2017-06-01 22:32:49 +0530

    Learning variables. Don't judge please :p

---
+++ CSharp101/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName;
+            String LastName;
+            DateTime Date_of_Birth;
+            String AddressLine1;
+            String AddressLine2;
+            String City;
+            String State;
+            int Pin;
+            String country;
+
+            FirstName = "Aditya";
+            Console.WriteLine(FirstName);
+
+        }
+    }
+}
