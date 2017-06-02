c1e75041065518e96446d7711c1ab52c5a58755b
Aditya Chetan <achetan40@gmail.com>
2017-06-03 00:24:05 +0530

    Hmm... Multi-return eh??

---
+++ CSharp103/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp103
+{
+    class Program
+    {	
+	public static bool multiReturn(out int i,out int j){
+		
+		i=0;
+		j=1;
+		return true;
+	
+	}
+
+
+        static void Main(string[] args)
+        {   int k,l;
+            Console.WriteLine("Hello World!");
+	    multiReturn(out k,out l);
+	    Console.WriteLine("{0},{1}",k.ToString(),l.ToString());
+
+        }
+    }
+}
