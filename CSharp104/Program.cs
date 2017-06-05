5f15c8072db23039300ac7395fdada583c380f92
Aditya Chetan <achetan40@gmail.com>
2017-06-05 12:44:52 +0530

    CSharp104: enum code not working, also need to research on enums

---
+++ CSharp104/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+		enum Days { Monday, Tuesday };
+		int x = Days.Monday;	
+		Console.WriteLine(x);
+	
+	}
+    }
+}
