d56cee8ca9984d08474a9e5bc40d89d5d79fa642
Aditya Chetan <achetan40@gmail.com>
2017-06-05 13:57:05 +0530

    Made a new struct, experimented with indexers. Still unsure about access modifiers

---
+++ CSharp106/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharp106
+{
+    public class Program
+    {	
+	public static void Main(string[] args){
+	
+		List myList = new List(3);
+		for(int i=0;i<myList.flowers.Length;i++){
+			
+			Console.WriteLine(myList.flowers[i]+" ");
+		
+		}
+		Console.WriteLine("\n");
+		
+		myList.flowers[0] = "JASMINE";
+
+		for(int i=0;i<myList.flowers.Length;i++){
+		
+			Console.WriteLine(myList.flowers[i]+" ");
+		
+		}
+
+
+	}
+	
+
+	
+    }
+
+    public struct List{
+    
+    	public string[] flowers;
+	
+	public List(int a){
+			
+		this.flowers = new string[] {"Lotus","Rose","Hibiscus"};
+		
+	}
+
+
+	public string this[int index]
+	{
+		get{ return this.flowers[index];}
+		set{ this.flowers[index] = value;}
+	
+	}
+    
+    }
+
+
+}
