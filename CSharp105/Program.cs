2a06dc0ed63fc3cecede0155560b351f3dd25577
Aditya Chetan <achetan40@gmail.com>
2017-06-05 12:54:41 +0530

    Exploring structs

---
+++ CSharp105/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp105
+{	
+    public struct coffee{
+    
+	public int strength;
+	public String Bean;
+
+	public coffee(int strength, String Bean){
+	
+		this.strength = strength;
+		this.Bean = Bean;
+	
+	}
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {	
+	    coffee espresso = new coffee(100,"cocoa");
+
+            Console.WriteLine(espresso.Bean);
+        }
+    }
+}
