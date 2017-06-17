181d2d1621c186def89094a128898d3cb21d7d32
Aditya Chetan <achetan40@gmail.com>
2017-06-17 20:11:23 +0530

    Played around with classes, class properties

---
+++ CSharp108/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharp108
+{
+    public class Program
+    {
+	private int age;
+
+	public string Name { get; set; }
+	public int Age {
+		get{
+			
+			return age; 
+			
+		}
+
+		set{
+		
+			age = value;
+		
+		}
+	}
+
+
+        static void Main(string[] args)
+        {
+		
+		var myProgram = new Program();
+		myProgram.Age = 24;
+		myProgram.Name = "John";
+
+		Console.WriteLine(myProgram.Age);
+
+        }
+    }
+}
